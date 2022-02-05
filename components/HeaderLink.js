0a91070a7c4e26b8baf92a8c2f67df4670568d17
Taha-0697 <chaghtai5.tc@gmail.com>
2022-02-05 13:14:24 +0500

    1:13pm Saturday 5-feb-2022

--- components/HeaderLink.js
+++ components/HeaderLink.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React from 'react'
 
-const HeaderLink = () => {
-  return( <div>
-      Done till 40 min
-  </div>);
-};
+const HeaderLink = (props) => {
+  const { text, Icon, avatar, feed } = props
+  return (
+    <div
+      className={`flex flex-col items-center justify-center cursor-pointer ${
+        feed
+          ? 'text-black opacity-60 hover:text-black dark:text-white opactiy-75 dark:hover:text-white lg:-mb-1.5 space-y-1'
+          : 'text-gray-500 hover:text-gray-700'
+      }`}
+    >
+      {avatar ? <Icon className="!h-7 !w-7 lg:!mb-1" /> : <Icon />}
+      <h4>{text}</h4>
+    </div>
+  )
+}
 
-export default HeaderLink;
+export default HeaderLink
